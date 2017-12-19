690d4b91d2683a06368e7b407db1222f33823546
Animelaw <33007106+Animelaw@users.noreply.github.com>
2017-12-19 16:24:55 +0700

    Update type.js

--- type.js
+++ type.js
@@ -132,7 +132,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "http://www.caranyakami.tech/p/go.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "http://law-safelink.blogspot.co.id/p/silakan-tekan-tombol-continue-untuk.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
